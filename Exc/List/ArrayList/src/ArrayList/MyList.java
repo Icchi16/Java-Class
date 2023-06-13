ddbd5fd4d08f8d63ed49f5cd352194fb7d75408b
SirIcchi <dynghiemhn16@gmail.com>
2023-06-13 11:09:13 +0700

    exc commit 3

--- Exc/List/ArrayList/src/ArrayList/MyList.java
+++ Exc/List/ArrayList/src/ArrayList/MyList.java
@@ -3,7 +3,6 @@ package ArrayList;
 import java.util.Arrays;
 
 public class MyList<E> {
-    private int size = 0;
     private static final int DEFAULT_CAPACITY = 10;
     private Object[] elements;
 
@@ -41,38 +40,54 @@ public class MyList<E> {
             elements[index] = null;
         }
         System.out.println("Removed: " + removedElement.toString());
+        System.out.println(Arrays.toString(elements));
         return removedElement;
     }
 
-    public int size(){
+    public int size() {
         return elements.length;
     }
 
-    public E clone(){
+    public E clone() {
         return null;
     }
 
-    public boolean contains(E object){
-        for (E element: elements
-             )
-            return element == object;
+    public boolean contains(E object) {
+        for (Object element : elements
+        )
+            if ((E) element == object) {
+                System.out.println("List contain " + object + " at index: " + indexOf(object));
+                return true;
+            }
+        ;
+        System.out.println("List not contain " + object);
         return false;
     }
 
-    public int indexOf(E object){
-        for ()
-    }
-
-
-
-
-
-    public E get(int i) {
-        if (i >= size || i < 0) {
-            throw new IndexOutOfBoundsException("Index: " + i + ", Size " + size);
+    public int indexOf(E object) {
+        for (int i = 0; i < elements.length; i++) {
+            if (object == (E) elements[i]) {
+                return i;
+            }
         }
-        return (E) elements[i];
+        return -1;
     }
 
 
+    public void ensureCapacity(int minCap) {
+        elements = Arrays.copyOf(elements, minCap);
+
+    }
+
+    public E get(int index) {
+        if (index >= elements.length || index < -1) {
+            return (E) elements[index];
+        }
+        throw new IndexOutOfBoundsException("Index out of bound | Index: " + index + " Size: " + elements.length);
+    }
+
+    public void clear() {
+        elements = new Object[elements.length];
+    }
 }
+
--- Exc/List/ArrayList/src/ArrayList/MyListTest.java
+++
@@ -1,7 +0,0 @@
-package ArrayList;
-
-public class MyListTest {
-    public static void main(String[] args) {
-        MyList<Integer> listInteger = new MyList<Integer>();
-    }
-}
